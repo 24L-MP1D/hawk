933bb9b9d234fadf288c5ae396815b152381a1b9
badral <badralbaigalmaa7@gmail.com>
2024-09-11 19:24:42 +0800

    category cards hover scale  finished

--- e-commerce-web/src/components/Card.tsx
+++ e-commerce-web/src/components/Card.tsx
@@ -16,8 +16,12 @@ export const Card = ({ cardItems }: { cardItems: cardItems }) => {
     }
   };
   return (
-    <div className="w-full flex flex-col gap-2 relative">
-      <div className="rounded-2xl bg-slate-400 aspect-[3/4]"></div>
+    <div className="w-full flex flex-col gap-2 relative group">
+      <div className="rounded-2xl bg-slate-400 aspect-[3/4] overflow-hidden">
+        <div className="group-hover:scale-150 bg-slate-50 transition-[10s]">
+          fafsdfdsa
+        </div>
+      </div>
       <div className="flex flex-col gap-1">
         <div>{cardItems.title}</div>
         <div className="font-bold">{cardItems.price}</div>
