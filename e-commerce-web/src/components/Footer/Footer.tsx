9337ad87a85edcd6c32d657ff160eb8bc6252e04
Галт Б <24LP0532@G401-CC0102.local>
2024-09-12 18:19:15 +0800

    updated footer

--- e-commerce-web/src/components/Footer/Footer.tsx
+++ e-commerce-web/src/components/Footer/Footer.tsx
@@ -11,19 +11,19 @@ import Link from "next/link";
 export const Footer = () => {
   return (
     <div className="container mx-auto bg-black text-white">
-      <div className="flex justify-between mx-52 mt-16 items-center">
+      <div className="flex justify-between mt-16 items-center">
         <div className="w-[41px] h-[35px]">
           <Image width={41} height={35} alt={"logo"} src={"/Pinecone@2x.png"} />
         </div>
         <div className="flex gap-10">
           <div className="flex items-center gap-4">
-            <div className="border rounded-full w-8 h-8 flex items-center justify-center text-slate-600">
+            <div className="border-[#FFFFFF2A] border-solid border-[1px] rounded-full w-8 h-8 flex items-center justify-center text-slate-600">
               <FiPhone className="text-white" />
             </div>
             <p>(976) 7007-1234</p>
           </div>
           <div className="flex items-center gap-4">
-            <div className="border rounded-full w-8 h-8 flex items-center justify-center">
+            <div className="border-[#FFFFFF2A] border-solid border-[1px] rounded-full w-8 h-8 flex items-center justify-center">
               <IoMailOutline />
             </div>
             <Link href="www.ecommerce.mn">contact@ecommerce.mn</Link>
@@ -31,9 +31,9 @@ export const Footer = () => {
         </div>
       </div>
 
-      <div className="flex items-center w-[1040px] h-[1.5px] bg-slate-50 mx-auto mt-11"></div>
+      <div className="flex items-center h-[1.5px] bg-[#FFFFFF1A] my-11"></div>
 
-      <div className="flex justify-between items-center mx-52 mb-16 mt-11">
+      <div className="flex justify-between items-center mb-16 ">
         <div className="text-[14px]">© 2024 Ecommerce MN</div>
         <div className="flex gap-7">
           <Link href="https://www.facebook.com/">
