90d98dbd8ed460de079fe112ebaf0dad9db3517c
Галт Б <24LP0532@G401-CC0102.local>
2024-09-13 19:48:57 +0800

    save heart

--- e-commerce-web/src/app/Save/page.tsx
+++ e-commerce-web/src/app/Save/page.tsx
@@ -1,65 +1,68 @@
+"use client";
+
 import Image from "next/image";
 import { FaHeart } from "react-icons/fa";
+import { useEffect, useState } from "react";
+import Link from "next/link";
+// import { HeartIcon } from "@radix-ui/react-icons";
+import { HeartIconSvg } from "@/components/HeartIcon";
 
 const save = [
   {
-    title: "",
-    image: "/",
-    price: "",
+    title: "Chunky Glyph Tee",
+    image: "/asian.png",
+    price: "120.000₮",
+  },
+  {
+    title: "Doodle Hoodie",
+    image: "/black.png",
+    price: "100.000₮",
+  },
+  {
+    title: "Local Styles Crewneck",
+    image: "/latino.png",
+    price: "80.000₮",
   },
 ];
 
 export default function Save() {
+  // const [heart, setHeart] = useState([]);
+
   return (
     <div className="max-w-[622px] mx-auto">
       <div>
         <p className="text-xl font-bold p-3">Хадгалсан бараа (3)</p>
-        <div className="flex justify-between pb-4">
-          <div className="flex gap-6">
-            <div>
-              <Image
-                src="/asian.png"
-                alt="image"
-                width={100}
-                height={100}
-                className="w-[100px] h-[100px] rounded-sm"
-              />
-            </div>
-            <div className="flex flex-col gap-1">
-              <p>Chunky Glyph Tee</p>
-              <p>120’000₮</p>
-              <button className="bg-[#2563EB] w-[81px] h-[28px] rounded-3xl text-sm text-white">
-                Сагслах
-              </button>
-            </div>
-          </div>
-          <div>
-            <FaHeart />
-          </div>
-        </div>
 
-        <div className="flex justify-between pb-4">
-          <div className="flex gap-6">
-            <div>
-              <Image
-                src="/asian.png"
-                alt="image"
-                width={100}
-                height={100}
-                className="w-[100px] h-[100px] rounded-sm"
-              />
+        <div className="flex flex-col gap-2 pb-4">
+          {save.map((title) => (
+            <div
+              key={title.title}
+              className="flex justify-between border-2 rounded-xl"
+            >
+              <div className="flex gap-6 p-4">
+                <Link href="/ProductTetails">
+                  <Image
+                    src={title.image}
+                    alt="image"
+                    width={100}
+                    height={100}
+                    className="w-[100px] h-[100px] rounded-xl"
+                  />
+                </Link>
+
+                <div className="flex flex-col gap-1">
+                  <Link href="/ProductTetails">{title.title}</Link>
+                  <p className="font-bold">{title.price}</p>
+                  <button className="bg-[#2563EB] w-[81px] h-[28px] rounded-3xl text-sm text-white">
+                    Сагслах
+                  </button>
+                </div>
+              </div>
+              <div className="flex p-4">
+                <HeartIconSvg fill={true} />
+              </div>
             </div>
-            <div className="flex flex-col gap-1">
-              <p>Chunky Glyph Tee</p>
-              <p>120’000₮</p>
-              <button className="bg-[#2563EB] w-[81px] h-[28px] rounded-3xl text-sm text-white">
-                Сагслах
-              </button>
-            </div>
-          </div>
-          <div>
-            <FaHeart />
-          </div>
+          ))}
         </div>
       </div>
     </div>
