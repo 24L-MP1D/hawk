15c1ae228b1c4efcaec683f7fd4529367a529371
Галт Б <24LP0532@G401-CC0102.local>
2024-09-17 17:55:47 +0800

    backend mongoose installation hiij bga

---
+++ e-commerce-service/src/config/mongodb.ts
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const connect = () => {
+    try {
+
+    } catch (error) {
+
+    }
+    
+}
--- e-commerce-service/src/index.ts
+++ e-commerce-service/src/index.ts
@@ -2,12 +2,28 @@
 import express from 'express';
 
 const app = express();
-const port = 3000;
+const port = 4500;
 
 app.get('/', (req, res) => {
-  res.send('Hello, TypeScript with Express!');
+  res.send('Hello, TypeScript with Express!GLT');
 });
+
+
+app.get('/products', (req, res)=>{
+  res.send([{name: "naraa"}]);
+});
+
+app.get('/products', (req, res)=>{
+  res.send([{
+    name: 'T-shirt',
+    price: 1000,
+    colors: ["red, yellow, white, black"]
+  }])
+})
+
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+
