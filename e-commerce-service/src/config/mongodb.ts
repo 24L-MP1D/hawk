2766496d60ba7cb7aa014ff3a9a3e184b8add85e
Галт Б <24LP0532@G401-CC0102.local>
2024-09-18 17:40:25 +0800

    user CRUD-iig backend deer hiij uzev

--- e-commerce-service/src/config/mongodb.ts
+++ e-commerce-service/src/config/mongodb.ts
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
 
-const connect = () => {
+export const connect = async () => {
     try {
-
+       await mongoose.connect("mongodb+srv://galtbatzana1:galt@cluster0.hcogk.mongodb.net/");
+       console.log("Connected to database successfully!Galt")
     } catch (error) {
-
+        console.error(error);
     }
     
-}
+};
--- e-commerce-service/src/index.ts
+++ e-commerce-service/src/index.ts
@@ -1,26 +1,15 @@
 // src/index.ts
 import express from 'express';
+import { connect } from './config/mongodb';
+import { userRouter } from './router/UserRouter';
 
 const app = express();
 const port = 4500;
 
-app.get('/', (req, res) => {
-  res.send('Hello, TypeScript with Express!GLT');
-});
+connect()
 
-
-app.get('/products', (req, res)=>{
-  res.send([{name: "naraa"}]);
-});
-
-app.get('/products', (req, res)=>{
-  res.send([{
-    name: 'T-shirt',
-    price: 1000,
-    colors: ["red, yellow, white, black"]
-  }])
-})
-
+app.use(express.json());
+app.use(userRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
