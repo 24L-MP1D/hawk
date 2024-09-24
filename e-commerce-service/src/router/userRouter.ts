b95b3fb334c45104ca17e2d6c139b8145240fe55
badral <badralbaigalmaa7@gmail.com>
2024-09-24 18:38:42 +0800

    fsa

--- e-commerce-service/src/router/userRouter.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createUser, getUsers, updateUser } from "../controller/UserController";
-
-export const userRouter = express.Router();
-
-userRouter
-  .post("/register", createUser)
-  .put("/updateUser/:id", updateUser)
-  .get("/getUsers", getUsers);
