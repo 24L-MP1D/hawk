97611aeee46be52a2064d5937d486d9206a3e93b
Галт Б <24LP0532@G401-CC0102.local>
2024-09-25 18:48:28 +0800

    nodemon

--- e-commerce-service/src/router/saveRouter.ts
+++ e-commerce-service/src/router/saveRouter.ts
@@ -1,15 +1,17 @@
-import  express  from "express";
-import { createSavedProducts, deleteSavedProducts, getSavedProducts, updateSavedProducts } from "../controller/save.Controller";
+import express from "express";
+import {
+  createSavedProducts,
+  deleteSavedProducts,
+  getSavedProducts,
+  updateSavedProducts,
+} from "../controller/save.Controller";
 
- 
 const saveRouter = express.Router();
- 
+
 saveRouter
-.get("/Save", getSavedProducts)
-.post("/Save", createSavedProducts)
-.put("/Save/:id", updateSavedProducts)
-.delete("/Save/:id",deleteSavedProducts)
- 
-export {saveRouter}
- 
- 
+  .get("/Save", getSavedProducts)
+  .post("/Save", createSavedProducts)
+  .put("/Save/:id", updateSavedProducts)
+  .delete("/Save/:id", deleteSavedProducts);
+
+export { saveRouter };
