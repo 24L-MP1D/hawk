c75370a4aa58ee818b61e3cefa311cd643c34c4b
Галт Б <24LP0532@G401-CC0102.local>
2024-09-26 19:43:48 +0800

    heart delete function

--- e-commerce-web/src/app/Save/page.tsx
+++ e-commerce-web/src/app/Save/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { motion } from "framer-motion";
 import Image from "next/image";
 import { FaHeart } from "react-icons/fa";
@@ -6,87 +7,79 @@ import { useEffect, useState } from "react";
 import Link from "next/link";
 // import { HeartIcon } from "@radix-ui/react-icons";
 import { HeartIconSvg } from "@/components/HeartIcon";
- 
-const save = [
-  {
-    title: "Chunky Glyph Tee",
-    image: "/asian.png",
-    price: "120.000₮",
-  },
-  {
-    title: "Doodle Hoodie",
-    image: "/black.png",
-    price: "100.000₮",
-  },
-  {
-    title: "Local Styles Crewneck",
-    image: "/latino.png",
-    price: "80.000₮",
-  },
-];
- 
+import { del } from "framer-motion/client";
+
+type card = {
+  name: string;
+  amount: number;
+  _id: string;
+};
+
 export default function Save() {
   const [heart, setHeart] = useState(true);
-  const [cards, setCards] = useState();
+  const [cards, setCards] = useState([]);
+
+  const loadCard = async () => {
+    const response = await fetch(`http://localhost:4000/Save`);
+    const data = await response.json();
+    setCards(data);
+  };
+
+  console.log(cards);
+  const deleteOneCard = async (id: string) => {
+    await fetch(`http://localhost:4000/Save/${id}`, {
+      method: "DELETE",
+    });
+    loadCard();
+  };
+
+  useEffect(() => {
+    loadCard();
+  }, []);
+
   // const savedCount = cards.length;
- 
-// useEffect(()=>{
-//    setHeart(true)
-// },[])
-console.log({heart})
+
   return (
     <div className="max-w-[622px] mx-auto">
       <div>
         <p className="text-xl font-bold p-3">Хадгалсан бараа (3)</p>
- 
+
         <div className="flex flex-col gap-2 pb-4">
-          {save.map((title) => (
-            <motion.div
-              key={title.title}
+          {cards.map((card: card) => (
+            <div
+              key={card._id}
               className="flex justify-between border-2 rounded-xl"
-              animate={{
-                y: heart ? 0 : 10,
-                opacity: heart ? 0 : 1
-              }}
-              initial={{
-                opacity: 0.1
-              }}
-              transition={{
-                duration: 2
-              }}
             >
               <div className="flex gap-6 p-4">
                 <Link href="/ProductTetails">
                   <Image
-                    src={title.image}
+                    src={cards.image}
                     alt="image"
                     width={100}
                     height={100}
                     className="w-[100px] h-[100px] rounded-xl"
                   />
                 </Link>
- 
+
                 <div className="flex flex-col gap-1">
-                  <Link href="/ProductTetails">{title.title}</Link>
-                  <p className="font-bold">{title.price}</p>
-                  <button onClick={()=>setHeart(false)} className="bg-[#2563EB] w-[81px] h-[28px] rounded-3xl text-sm text-white">
+                  <Link href="/ProductTetails">{card.name}</Link>
+                  <p className="font-bold">{card.amount}</p>
+                  <button className="bg-[#2563EB] w-[81px] h-[28px] rounded-3xl text-sm text-white">
                     Сагслах
                   </button>
                 </div>
               </div>
               <div className="flex p-4">
-                <FaHeart onClick={()=>setHeart(!heart)}/>
-               
+                <FaHeart onClick={() => deleteOneCard(card._id)} />
               </div>
-            </motion.div>
+            </div>
           ))}
         </div>
       </div>
     </div>
   );
 }
- 
-{/* <HeartIconSvg fill={true} /> */}
- 
- 
- 
+
+{
+  /* <HeartIconSvg fill={true} /> */
+}
