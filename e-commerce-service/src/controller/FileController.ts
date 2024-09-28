137106e82f0b56a4ca19b4b41a43115e6f600bd0
badral <badral@badrals-MacBook-Pro.local>
2024-09-28 15:14:58 +0800

    f

--- e-commerce-service/src/controller/FileController.ts
+++ e-commerce-service/src/controller/FileController.ts
@@ -6,7 +6,7 @@ const fileUploader = async (req: Request, res: Response) => {
 
   try {
     const b64 = Buffer.from(req.file.buffer).toString("base64");
-    let dataURI = "data:" + req.file.mimetype + ";base64" + b64;
+    let dataURI = `data:${req.file.mimetype};base64,${b64}`;
     const cldRes = await handleUpload(dataURI);
     res.json(cldRes);
   } catch (error) {
--- e-commerce-service/src/router/uploadRouter.ts
+++ e-commerce-service/src/router/uploadRouter.ts
@@ -1,6 +1,6 @@
 import express from "express";
-import { fileUploader } from "../controller/FileController";
 import Multer, { memoryStorage } from "multer";
+import { fileUploader } from "../controller/FileController";
 const uploadRouter = express.Router();
 const storage = memoryStorage();
 const multer = Multer({ storage });
