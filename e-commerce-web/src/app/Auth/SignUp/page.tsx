1485eba356b899c6cd19a286f3e9b449ebffe76c
Galtbatzana <164704929+Galtbatzana@users.noreply.github.com>
2024-09-30 10:44:35 +0800

    finished front

--- e-commerce-web/src/app/Auth/SignUp/page.tsx
+++ e-commerce-web/src/app/Auth/SignUp/page.tsx
@@ -5,8 +5,8 @@ import { FaArrowRightLong } from "react-icons/fa6";
 
 export default function Page() {
     return (
-        <div className="flex flex-col">
-            <div className="flex justify-between">
+        <div className="flex flex-col vh-100%">
+            <div className="flex justify-between mb-[100px] relative z-10">
                 <div>
                     <img src="/Group.png" alt="logo" className="width-[20px] height-[10px}"/>
                 </div>
@@ -36,8 +36,8 @@ export default function Page() {
                 </div>
             </div>
 
-            <div className="flex flex-col mx-auto w-[440px] items-center border-2">
-                <p className="text-[#121316] text-[32px] font-bold my-10">Бүртгүүлэх</p>
+            <div className="flex flex-col mx-auto w-[440px] items-center border-2 rounded-lg mb-[100px]">
+                <p className="text-[#121316] text-[32px] font-extrabold my-8">Бүртгүүлэх</p>
                 <div className="min-w-[360px] my-3">
                     <p className="mb-2">Таны имэйл</p>
                     <input placeholder="Имэйл" className="bg-[#F7F7F8] min-w-[360px] h-[56px] border-2 pl-2 rounded-md"></input>
@@ -50,9 +50,28 @@ export default function Page() {
                     Дараах 
                     <FaArrowRightLong className="text-white"/>
                 </button>
+                <div className="min-w-[360px] border-b-2 my-4"></div>
                 
+                <button className="flex items-center bg-[#F7F7F8] border-2 min-w-[360px] h-[56px] my-3 justify-center">
+                    <img src="/_CompanyLogo.png" alt="logo" className="w-[20px] h-[20px] m-[17px]"/>
+                    <p>Google-ээр нэвтрэх</p>
+                </button>
+                <button className="flex items-center bg-[#F7F7F8] border-2 min-w-[360px] h-[56px] my-3 justify-center">
+                    <img src="/windows.png" alt="logo" className="w-[20px] h-[20px] m-[17px]"/>
+                    <p>Microsoft-оор нэвтрэх</p>
+                </button>
+                <button className="flex items-center bg-[#F7F7F8] border-2 min-w-[360px] h-[56px] my-3 justify-center">
+                    <img src="/apple.png" alt="logo" className="w-[20px] h-[20px] m-[17px]"/>
+                    <p>Apple-ээр нэвтрэх</p>
+                </button>
+                <div className="min-w-[360px] border-b-2 my-4"></div>
+                <div className="flex items-center min-w-[360px] h-[56px] mt-6 mb-10 justify-center">
+                    <p className="text-[#525252]">Бүртгэлтэй юү?</p>
+                    <p className="underline ml-2">Нэвтрэх</p>
+                </div>
             </div>
 
+            <div className="text-[#a2a2b2] flex mx-auto my-[14px]">© 2023 Pinecone</div>
         </div>
     );
 } 
