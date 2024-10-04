44693463c29b08de38560dc1dac2c632767cb8d3
Galtbatzana <164704929+Galtbatzana@users.noreply.github.com>
2024-10-04 13:56:14 +0800

    dash login token

--- e-commerce-service/src/index.ts
+++ e-commerce-service/src/index.ts
@@ -3,8 +3,8 @@
 import express from "express";
 import connectDB from "./configs/database";
 import cors from "cors";
-
-
+import jwt from 'jsonwebtoken';
+import bcrypt from "bcrypt";
 import {
   createProduct,
   deleteProducts,
@@ -34,6 +34,30 @@ app.use(userRouter);
 app.use(saveRouter);
 
 app.post("/Auth/SignUp", createUserSignUp);
+
+app.post("/signin", (req: Request, res: Response) => {
+const {email, password} = req.body;
+const isAuthenticated = true;  // DB user fetch compare
+
+const saltRounds = 10;
+const salt = bcrypt.genSaltSync(saltRounds);
+const hashedPass = bcrypt.hashSync(password.password, salt);
+
+const form = {
+    email: email.email,
+    password: hashedPass,
+  };
+  console.log({ form });
+
+if(isAuthenticated) {
+  const privateKey = "1234"; // .env file deeree nuuna
+  const token = jwt.sign({ email: email }, privateKey, { expiresIn: '2h' }); 
+
+return res.json({token})
+} else {
+return res.sendStatus(401);
+}
+})
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
