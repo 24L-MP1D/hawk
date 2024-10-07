7e5360391a7e94302d17965f0aecda52a9173e54
Oyundari23 <dariaa0560@gmail.com>
2024-10-07 15:19:51 +0800

    edit

--- dashboard-front-end/src/app/dashboard/income/page.tsx
+++ dashboard-front-end/src/app/dashboard/income/page.tsx
@@ -1,9 +1,7 @@
 "use client"; // usestate ashiglaj bolohgui bsn
 import { Calendar, ChevronDown, Download } from "lucide-react";
-import Dashboard from "../dashboard/page";
-import { useRef, useState } from "react";
+import { useState } from "react";
 import { DatePickerWithRange } from "@/components/DatePickerWithRange.tsx";
-import { PopoverContent } from "@radix-ui/react-popover";
 import { DashboardAside } from "@/components/Dashboard";
 
 const Income = () => {
@@ -57,38 +55,6 @@ const Income = () => {
           <DatePickerWithRange isDatePickerOpen={isDatePickerOpen} />
         </div>
       </div>
-
-      {/* <div className="w-[724px] h-[160px] bg-[#FFFFFF] rounded-2xl border-[1px] border-[#ECEDF0] ml-[186.5px] flex flex-col  mt-[16px]">
-<div className="flex justify-between p-[24px] ">
-<div className="font-bold text-[20px] ">Орлого</div>
-<div className="w-[144px] h-[28px] bg-[#1C20240A] rounded-md flex gap-2 px-[6px] py-[2px]">
-<div>
-<Download className="h-[20px] w-[20px]" />{" "}
-</div>
-<div className="text-[14px] "> Хуулга татах</div>
-</div>
-</div>
-<div className="border-[#ECEDF0] "></div>
-<div className="p-[24px] flex justify-between ">
-<div className="font-bold text-[28px]">235,000.00 MNT</div>
-<div className="flex gap-2">
-<div className="w-[82px] h-[36px] py-1.5 px-3 rounded-md text-[14px]  border-[#ECEDF0] border-[1px]">
-              Өнөөдөр
-</div>
-<div className="text-white bg-[#18BA51] w-[94px] h-[36px] py-2 px-4 rounded-md text-[14px] font-bold">
-              7 хоног
-</div>
-<div className=" w-[132px] h-[36px] py-1.5 px-3 rounded-md   border-[#ECEDF0] border-[1px] flex gap-3">
-<Calendar />
-<div className="text-[14px]">Сараар</div>
- 
-              <ChevronDown />
- 
-              <DatePickerWithRange isDatePickerOpen={isDatePickerOpen} />
-</div>
-</div>
-</div>
-</div> */}
     </div>
   );
 };
