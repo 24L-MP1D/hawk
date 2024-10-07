8d3aa8b4c1633283c8d3fda0c255719b703195e9
Galtbatzana <164704929+Galtbatzana@users.noreply.github.com>
2024-10-07 19:54:37 +0800

    token butsaah findOne string baih yostoi

--- e-commerce-service/src/controller/loginController.ts
+++ e-commerce-service/src/controller/loginController.ts
@@ -1,29 +1,39 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { Response, Request } from "express";
-const mockUser = {
-  email: "badral@gmail.com",
-  password: "Galt@123",
-};
+import { UserSignUpModel } from "../model/SignUpModel";
+// const mockUser = {
+//   email: "badral@gmail.com",
+//   password: "Galt@123",
+// };
 
 export const login = async (req: Request, res: Response) => {
   const { email, password } = req.body;
-  // const isAuthenticated =
-  //   email === mockUser.email && bcrypt.compareSync(password, mockUser.password);
-  const isAuthenticated =
-    email === mockUser.email && password === mockUser.password;
-  try {
-    if (isAuthenticated) {
-      const privateKey = "1234"; // .env file deeree nuuna
-      const token = jwt.sign({ email: email }, privateKey, {
-        expiresIn: "2h",
-      });
-      console.log({ token });
-      return res.json({ token });
+  const user = await UserSignUpModel.findOne({ email });
+  console.log({ user });
+
+  if (user) {
+    const isAuthenticated =
+      email === user.email &&
+      bcrypt.compareSync(password, String(user.password));
+
+    // const isAuthenticated = email === user.email && password === user.password;
+    try {
+      if (isAuthenticated) {
+        const privateKey = "1234"; // .env file deeree nuuna
+        const token = jwt.sign({ email: email }, privateKey, {
+          expiresIn: "2h",
+        });
+        console.log({ token });
+        return res.json({ token });
+      } else {
+        return res.status(401).json({ message: "Wrong pass" });
+      }
+    } catch (error) {
+      return res.status(401).json({ message: "Unauthorized user" });
     }
-  } catch (error) {
-    return res.status(401).json({ message: "Unauthorized user" });
   }
+  return res.status(401).json({ message: "No user" });
 };
 
 // const login = async (req: Request, res: Response) => {
