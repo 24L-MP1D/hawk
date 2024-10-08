0e26547359b3a075a8c9dfff53eac25dd4febc9b
Galtbatzana <164704929+Galtbatzana@users.noreply.github.com>
2024-10-08 20:01:55 +0800

    cloudinary BD ruu image oruulsan

--- e-commerce-service/src/configs/cloudinary.ts
+++ e-commerce-service/src/configs/cloudinary.ts
@@ -1,3 +1,26 @@
+import { v2 as cloudinary } from "cloudinary";
+import "dotenv/config";
+
+try {
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARYNAME,
+    api_key: process.env.CLOUDINARYKEY,
+    api_secret: process.env.CLOUDINARYSECRETKEY,
+  });
+  console.log("success image upload");
+} catch (err) {
+  console.log(err);
+}
+
+export const handleUpload = async (file: string) => {
+  const res = await cloudinary.uploader.upload(file, {
+    resource_type: "auto",
+  });
+  return res;
+};
+
+/////////////////////////////////////////////////////
+// // galt-iin cloudinary tai holbolt hiisen bgaa ni
 // import { v2 as cloudinary } from "cloudinary";
 // import "dotenv/config";
 
@@ -7,9 +30,8 @@
 //     api_key: process.env.CLOUDINARYKEY,
 //     api_secret: process.env.CLOUDINARYSECRETKEY,
 //   });
-//   console.log("success image upload");
-// } catch (err) {
-//   console.log(err);
+// } catch (error) {
+//   console.log(error);
 // }
 
 // export const handleUpload = async (file: string) => {
@@ -18,23 +40,4 @@
 //   });
 //   return res;
 // };
-
-import { v2 as cloudinary } from "cloudinary";
-import "dotenv/config";
-
-try {
-  cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.API_KEY,
-    api_secret: process.env.API_SECRET,
-  });
-} catch (error) {
-  console.log(error);
-}
-
-export const hadleUpload = async (file: string) => {
-  const res = await cloudinary.uploader.upload(file, {
-    resource_type: "auto",
-  });
-  return res;
-};
+// // cloudinary ruu file ilgeej baigaa function
--- e-commerce-service/src/configs/cloudinary1.ts
+++ e-commerce-service/src/configs/cloudinary1.ts
@@ -1,19 +1,22 @@
-// import { v2 as cloudinary } from "cloudinary";
-// import "dotenv/config";
+import { v2 as cloudinary } from "cloudinary";
+import "dotenv/config";
 
-// try {
-//   cloudinary.config({
-//     cloud_name: process.env.CLOUD_NAME,
-//     api_key: process.env.API_KEY,
-//     api_secret: process.env.API_SECRET,
-//   });
-// } catch (error) {
-//   console.log(error);
-// }
+// galt-iin cloudinary tai holbolt hiisen bgaa ni
 
-// export const hadleUpload = async (file: string) => {
-//   const res = await cloudinary.uploader.upload(file, {
-//     resource_type: "auto",
-//   });
-//   return res;
-// };
+try {
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARYNAME,
+    api_key: process.env.CLOUDINARYKEY,
+    api_secret: process.env.CLOUDINARYSECRETKEY,
+  });
+} catch (error) {
+  console.log(error);
+}
+
+export const handleUpload = async (file: string) => {
+  const res = await cloudinary.uploader.upload(file, {
+    resource_type: "auto",
+  });
+  return res;
+};
+// cloudinary ruu file ilgeej baigaa function
