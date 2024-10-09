28ab26098a334a5f142c7dcb859df4c704c564e6
badral <badral@badrals-MacBook-Pro.local>
2024-10-09 14:53:24 +0800

    bdrl

--- e-commerce-service/src/controller/loginController.ts
+++ e-commerce-service/src/controller/loginController.ts
@@ -1,32 +1,25 @@
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import { Response, Request } from "express";
-import { UserSignUpModel } from "../model/SignUpModel";
+import { Request, Response } from "express";
+import { User } from "../model/UserModel";
 
-export const login = async (req: Request, res: Response) => {
-  const { email, password } = req.body;
-  const user = await UserSignUpModel.findOne({ email });
-  console.log({ user });
+export const loginUser = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
 
-  if (user) {
-    const isAuthenticated =
-      email === user.email &&
-      bcrypt.compareSync(password, String(user.password));
-
-    try {
-      if (isAuthenticated) {
-        const privateKey = "1234"; // .env file deeree nuuna
-        const token = jwt.sign({ email: email }, privateKey, {
-          expiresIn: "2h",
-        });
-        console.log({ token });
-        return res.json({ token });
-      } else {
-        return res.status(401).json({ message: "Wrong pass" });
-      }
-    } catch (error) {
-      return res.status(401).json({ message: "Unauthorized user" });
+    const user = await User.findOne({ email });
+    if (!user) return res.status(401).send("user does not exist");
+    if (user.password !== password)
+      return res.status(401).send("password incorrect");
+    else {
+      return res.status(201).send(user);
     }
+
+    // if (password === password) return res.status(201).send(user);
+    // else return res.status(401).send("password incorrect");
+
+    // const isEqual = bcrypt(String(password), user.password);
+    // if (isEqual) return res.send(user);
+    // res.status(401).send("password incorrect");
+  } catch (error) {
+    res.send("aldaaaa");
   }
-  return res.status(401).json({ message: "No user" });
 };
